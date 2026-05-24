36fb51fde8fc3578c86cf3a85fcb6ac9f283384b
miguel <miguel.compina1@gmail.com>
2026-05-24 17:44:39 -0500

    commit inicial

---
+++ src/main/java/com/mhuallpa/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.mhuallpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/saludo")
+	public String saludar(){
+		return "Hola, Sigueme en LinkedIn!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
